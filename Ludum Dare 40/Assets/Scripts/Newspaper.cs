34557d4c18e6342e357f53a69948f597d8a8d707
Jacob Bashista <tgb.bashista@gmail.com>
2017-12-02 16:20:12 -0500

    Heater

--- Ludum Dare 40/Assets/Scripts/Newspaper.cs
+++ Ludum Dare 40/Assets/Scripts/Newspaper.cs
@@ -10,12 +10,22 @@ public class Newspaper : MonoBehaviour {
 
 	bool inStack;
 
+
+    public GameObject heatBit;
+
+
+    public float heat;
+
+    bool heatUp;
+
+
     GameController gameManager;
 
 	void Start(){
 		rend = GetComponent<Renderer> ();
 		baseColor = rend.material.color;
         gameManager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameController>();
+        rend.material.SetColor("_EMISSION", Color.red);
 	}
 
 	void OnMouseOver(){
@@ -25,8 +35,35 @@ public class Newspaper : MonoBehaviour {
 		rend.material.color = baseColor;
 	}
 
-	void FixedUpdate(){
-        
+	void Update(){
+        if (heatUp)
+        {
+            heat += Time.deltaTime * 2;
+        }
+        else if(heat > 0){
+            heat -= Time.deltaTime * 2;
+        }
+
+        rend.material.SetColor("_EmissionColor", new Vector4(heat/100,0,0,heat/100));
+
+
+        if(heat > 20){
+            heatBit.SetActive(true);
+        }else{
+            heatBit.SetActive(false);
+        }
+
+
+        if(heat > 100){
+            if (!gameManager.hasDelivered)
+            {
+                gameManager.deliveryTime -= 1;
+            }
+            gameManager.sleepTime -= 1;
+            Destroy(gameObject);
+        }
+
+
 	}
 
 
@@ -44,6 +81,15 @@ public class Newspaper : MonoBehaviour {
             }
             gameManager.sleepTime -= 1;
             Destroy(gameObject);
+        }
+        if(other.gameObject.tag == "Heater"){
+            heatUp = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.tag == "Heater"){
+            heatUp = false;
         }
     }
 
