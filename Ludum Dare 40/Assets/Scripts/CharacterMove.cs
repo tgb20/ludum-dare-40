7ac60e9c331b5cbf015d0e14de06efee2fbec880
Jacob Bashista <tgb.bashista@gmail.com>
2017-12-02 08:24:12 -0500

    Basic Character and Paper Pickup

---
+++ Ludum Dare 40/Assets/Scripts/CharacterMove.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMove : MonoBehaviour {
+
+    public float speed = 10.0f;
+    public float gravity = 10.0f;
+    public float maxVelocityChange = 10.0f;
+    public bool canJump = true;
+    public float jumpHeight = 2.0f;
+    private bool grounded = false;
+
+
+    Rigidbody rigi;
+
+    void Awake()
+    {
+        rigi = GetComponent<Rigidbody>();
+
+        rigi.freezeRotation = true;
+        rigi.useGravity = false;
+    }
+
+    void FixedUpdate()
+    {
+        if (grounded)
+        {
+            // Calculate how fast we should be moving
+            Vector3 targetVelocity = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            targetVelocity = transform.TransformDirection(targetVelocity);
+            targetVelocity *= speed;
+
+            // Apply a force that attempts to reach our target velocity
+            Vector3 velocity = rigi.velocity;
+            Vector3 velocityChange = (targetVelocity - velocity);
+            velocityChange.x = Mathf.Clamp(velocityChange.x, -maxVelocityChange, maxVelocityChange);
+            velocityChange.z = Mathf.Clamp(velocityChange.z, -maxVelocityChange, maxVelocityChange);
+            velocityChange.y = 0;
+            rigi.AddForce(velocityChange, ForceMode.VelocityChange);
+
+            // Jump
+            if (canJump && Input.GetButton("Jump"))
+            {
+                rigi.velocity = new Vector3(velocity.x, CalculateJumpVerticalSpeed(), velocity.z);
+            }
+        }
+
+        // We apply gravity manually for more tuning control
+        rigi.AddForce(new Vector3(0, -gravity * rigi.mass, 0));
+
+        grounded = false;
+    }
+
+    void OnCollisionStay()
+    {
+        grounded = true;
+    }
+
+    float CalculateJumpVerticalSpeed()
+    {
+        // From the jump height and gravity we deduce the upwards speed 
+        // for the character to reach at the apex.
+        return Mathf.Sqrt(2 * jumpHeight * gravity);
+    }
+}
---
+++ Ludum Dare 40/Assets/Scripts/PickupPapers.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupPapers : MonoBehaviour {
+
+
+    public GameObject playerCamera;
+    bool carrying;
+    public float distance;
+    public float smooth;
+
+    GameObject carriedPaper;
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(carrying){
+            carry(carriedPaper);
+            checkDrop();
+        } else{
+            pickup();
+        }
+	}
+
+
+    void carry(GameObject p){
+        p.transform.position = Vector3.Lerp(p.transform.position, playerCamera.transform.position + playerCamera.transform.forward * distance, Time.deltaTime * smooth);
+    }
+    void pickup(){
+        if(Input.GetMouseButtonDown(0)){
+            int x = Screen.width / 2;
+            int y = Screen.height / 2;
+
+            Ray ray = playerCamera.GetComponent<Camera>().ScreenPointToRay(new Vector3(x, y));
+
+            RaycastHit hit;
+
+            if(Physics.Raycast(ray, out hit)){
+                Newspaper p = hit.collider.GetComponent<Newspaper>();
+                if(p != null){
+                    carrying = true;
+                    carriedPaper = p.gameObject;
+                    p.GetComponent<Rigidbody>().isKinematic = true;
+                }
+            }
+
+
+        }
+    }
+
+
+    void checkDrop(){
+        if(Input.GetMouseButtonDown(0)){
+            dropPaper();
+        }
+    }
+
+    void dropPaper(){
+        carrying = false;
+        carriedPaper.GetComponent<Rigidbody>().isKinematic = false;
+        carriedPaper = null;
+    }
+
+
+
+}
